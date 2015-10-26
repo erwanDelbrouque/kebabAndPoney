c2d47d3acea4efba59230071d88db894a4a4001d
Maws <mnezzari@gmail.com>
2015-10-26 14:19:38 +0100

    Ajout des classes BasketResourcefulUser et CubicleResourcefulUser qui hérite de ResourcefulUser et modification de la classe Swimmer

---
+++ Project 2 Actions and Pools/src/model/ressources/BasketResourcefulUser.java
@@ -0,0 +1,13 @@
+package model.ressources;
+
+public class BasketResourcefulUser extends ResourcefulUser<Basket> {
+	
+	public BasketResourcefulUser(String name) {
+		super(name);
+	}
+	
+	public BasketResourcefulUser() {
+		super("Basket User");
+	}
+
+}
