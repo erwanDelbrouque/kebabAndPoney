c254f0f0f7dc148e03a35078926485ca1cbec868
erwandelbrouque <erwandelbrouque@gmail.com>
2015-10-30 15:53:28 +0100

    Ajout du test pour créer un swimmer

--- Project 2 Actions and Pools/tests/scheduler/SwimmerTest.java
+++ Project 2 Actions and Pools/tests/scheduler/SwimmerTest.java
@@ -14,34 +14,41 @@ public class SwimmerTest extends SequentialSchedulerTest {
 	public void negatifTimeToUndressTest() throws ActionFinishedException, ActionInProgressException, IllegalArgumentException{
 		BasketPool baskets = new BasketPool(6);
 		CubiclePool cubicles = new CubiclePool(3);
-		Swimmer mockedSwimmer = new Swimmer("test",baskets, cubicles, -1, 6, 5);
+		new Swimmer("test",baskets, cubicles, -1, 6, 5);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void negatifTimeTodressTest() throws ActionFinishedException, ActionInProgressException, IllegalArgumentException{
 		BasketPool baskets = new BasketPool(6);
 		CubiclePool cubicles = new CubiclePool(3);
-		Swimmer mockedSwimmer = new Swimmer("test",baskets, cubicles, 4, 6, -1);
+		new Swimmer("test",baskets, cubicles, 4, 6, -1);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void negatifTimeToBatheTest() throws ActionFinishedException, ActionInProgressException, IllegalArgumentException{
 		BasketPool baskets = new BasketPool(6);
 		CubiclePool cubicles = new CubiclePool(3);
-		Swimmer mockedSwimmer = new Swimmer("test",baskets, cubicles, 4, -1, 5);
+		new Swimmer("test",baskets, cubicles, 4, -1, 5);
 	}
 	
 	@Test(expected = NullPointerException.class)
 	public void BasketPoolNullTest() throws ActionFinishedException, ActionInProgressException, IllegalArgumentException{
 		BasketPool baskets = null;
 		CubiclePool cubicles = new CubiclePool(3);
-		Swimmer mockedSwimmer = new Swimmer("test",baskets, cubicles, 4, 1, 5);
+		new Swimmer("test",baskets, cubicles, 4, 1, 5);
 	}
 	
 	@Test(expected = NullPointerException.class)
 	public void cubiclePoolNullTest() throws ActionFinishedException, ActionInProgressException, IllegalArgumentException{
 		BasketPool baskets = new BasketPool(3);
 		CubiclePool cubicles = null;
-		Swimmer mockedSwimmer = new Swimmer("test",baskets, cubicles, 4, 1, 5);
+		new Swimmer("test",baskets, cubicles, 4, 1, 5);
+	}
+	
+	@Test
+	public void createSwimmerTest() throws IllegalArgumentException, NullPointerException, ActionFinishedException, ActionInProgressException{
+		BasketPool baskets = new BasketPool(3);
+		CubiclePool cubicles = new CubiclePool(3);
+		new Swimmer("test",baskets, cubicles, 4, 1, 5);
 	}
 }
