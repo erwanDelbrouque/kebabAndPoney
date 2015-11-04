5b7a874b2489ee02765e7e0190f61b0e2ea3ecfb
Togam <margot_six@hotmail.com>
2015-11-04 19:07:09 +0100

    Ajout du test fairSchedulerInFairScheduler qui teste le déroulement d'un fair scheduler qui contient lui même un autre fair scheduler

--- Project 2 Actions and Pools/tests/scheduler/FairSchedulerTest.java
+++ Project 2 Actions and Pools/tests/scheduler/FairSchedulerTest.java
@@ -61,7 +61,99 @@ public class FairSchedulerTest extends SchedulerTest {
 	}
 	
 	//TODO : Add a test to test a scheduler in a scheduler
-	
+	@Test 
+	public void fairSchedulerInFairScheduler() throws ActionFinishedException, ActionInProgressException{
+		Scheduler scheduler1 = new FairScheduler();
+		Scheduler scheduler2 = new FairScheduler();
+		
+		Action a1 = new ForeseeableAction(2);
+		Action a2 = new ForeseeableAction(2);
+		Action a3 = new ForeseeableAction(2);
+		Action a4 = new ForeseeableAction(2);
+		
+		scheduler1.addActions(a1,a2);
+		// one fairScheduler inside another fairScheduler
+		scheduler2.addActions(a3,scheduler1,a4);
+		
+		isReadyTest(scheduler2);
+		isReadyTest(a3);
+		isReadyTest(scheduler1);
+		isReadyTest(a1);
+		isReadyTest(a2);
+		isReadyTest(a4);
+		
+		scheduler2.doStep();
+		
+		isInProgressTest(scheduler2);
+		isInProgressTest(a3);
+		isReadyTest(scheduler1);
+		isReadyTest(a1);
+		isReadyTest(a2);
+		isReadyTest(a4);
+		
+		scheduler2.doStep();
+		
+		isInProgressTest(scheduler2);
+		isInProgressTest(a3);
+		isInProgressTest(scheduler1);
+		isInProgressTest(a1);
+		isReadyTest(a2);
+		isReadyTest(a4);
+		
+		scheduler2.doStep();
+		
+		isInProgressTest(scheduler2);
+		isInProgressTest(a3);
+		isInProgressTest(scheduler1);
+		isInProgressTest(a1);
+		isReadyTest(a2);
+		isInProgressTest(a4);
+		
+		scheduler2.doStep();
+		
+		isInProgressTest(scheduler2);
+		isFinishedTest(a3);
+		isInProgressTest(scheduler1);
+		isInProgressTest(a1);
+		isReadyTest(a2);
+		isInProgressTest(a4);
+		
+		scheduler2.doStep();
+		
+		isInProgressTest(scheduler2);
+		isFinishedTest(a3);
+		isInProgressTest(scheduler1);
+		isInProgressTest(a1);
+		isInProgressTest(a2);
+		isInProgressTest(a4);
+		
+		scheduler2.doStep();
+		
+		isInProgressTest(scheduler2);
+		isFinishedTest(a3);
+		isInProgressTest(scheduler1);
+		isInProgressTest(a1);
+		isInProgressTest(a2);
+		isFinishedTest(a4);
+		
+		scheduler2.doStep();
+		
+		isInProgressTest(scheduler2);
+		isFinishedTest(a3);
+		isInProgressTest(scheduler1);
+		isFinishedTest(a1);
+		isInProgressTest(a2);
+		isFinishedTest(a4);
+		
+		scheduler2.doStep();
+		
+		isFinishedTest(scheduler2);
+		isFinishedTest(a3);
+		isFinishedTest(scheduler1);
+		isFinishedTest(a1);
+		isFinishedTest(a2);
+		isFinishedTest(a4);
+	}
 	
 	
 
